5316a55dcb730191004b26a6498f9aa6f5226327
Philip Bower <philbower80@outlook.com>
2020-06-12 18:41:49 +0100

    added Head info and links to font awesome, bootstrap and added favicon.

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php $title = "Philip Bower" ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" href="Assets\favicon-32x32.png" type="image/x-icon">
+  <script src="https://kit.fontawesome.com/0a4ce35ecc.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+  <!-- <link rel="stylesheet" href="css/normalize.css"> -->
+  <link rel="stylesheet" href="css/styles.css">
+  <title><?php echo $title ?></title>
+</head>
+
+  <body>
+
+    <?php include("inc/header.php"); ?>
+
+    <section class="main">
+      <div>
+      THIS IS THE MAIN SECTION
+      </div>
+    </section>
+
+    <?php include("inc/footer.php"); ?>
+    
+  </body>
+</html>
